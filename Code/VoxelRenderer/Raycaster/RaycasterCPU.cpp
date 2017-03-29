7849502bcc4f6900c43432b6f8b596e2046e9fdb
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-03-29 18:05:39 +0200

    Raycaster on CPU start

--- Code/VoxelRenderer/Raycaster/RaycasterCPU.cpp
+++ Code/VoxelRenderer/Raycaster/RaycasterCPU.cpp
@@ -1,21 +1,102 @@
 #include "RaycasterCPU.h"
 
 
+#include <iostream>
+
+
 namespace vr
 {
 
 
+// ================================ //
+//
+RaycasterCPU::RaycasterCPU()
+	:	m_height( 0 )
+	,	m_width( 0 )
+	,	m_renderer( nullptr )
+	,	m_resourceManager( nullptr )
+	,	m_run( false )
+	,	m_end( false )
+{}
 
 // ================================ //
 //
 void			RaycasterCPU::Init			( IRenderer* renderer, ResourceManager* resourceManager )
-{}
+{
+	PrepareThreads();
+
+}
 
 
 // ================================ //
 //
 void			RaycasterCPU::Render		( OctreePtr octree, RenderTargetObject* svoRenderTarget, CameraActor* camera )
-{}
+{
+	if( m_width != camera->GetWidth() || m_height != camera->GetHeight() )
+		ReallocateRenderBuffer( camera->GetWidth(), camera->GetHeight() );
+
+	SpawnThreads( octree, camera );
+	UpdateRenderTarget( m_renderBuffer.get(), svoRenderTarget );
+}
+
+// ================================ //
+//
+void			RaycasterCPU::SpawnThreads				( OctreePtr octree, CameraActor* camera )
+{
+	// Run threads
+	m_run = true;
+	m_condition.notify_all();
+
+	m_run = false;
+}
+
+// ================================ //
+//
+void			RaycasterCPU::ThreadWait()
+{
+	while( !m_run )
+		m_condition.wait( m_lock );
+
+	// Check it to be sure.
+	if( !m_run )
+		std::cout << "Spurious wake up!\n";
+}
+
+// ================================ //
+//
+void			RaycasterCPU::RaycasterThread			( OctreePtr octree, CameraActor* camera, uint32 startRange, uint32 endRange )
+{
+	ThreadWait();
+
+
+
+}
+
+// ================================ //
+//
+void			RaycasterCPU::PrepareThreads()
+{
+	auto numThreads = std::thread::hardware_concurrency();
+	assert( numThreads );
+	m_threadPool.reserve( numThreads );
+}
+
+// ================================ //
+//
+void			RaycasterCPU::ReallocateRenderBuffer	( uint16 newWidth, uint16 newHeight )
+{
+	m_width = newWidth;
+	m_height = newHeight;
+
+	m_renderBuffer = std::unique_ptr< uint32[] >( new uint32[ m_height * m_width ] );
+}
+
+// ================================ //
+//
+void			RaycasterCPU::UpdateRenderTarget		( uint32* buffer, RenderTargetObject* svoRenderTarget )
+{
+	assert( false );
+}
 
 
 }	// vr
--- Code/VoxelRenderer/Raycaster/RaycasterCPU.h
+++ Code/VoxelRenderer/Raycaster/RaycasterCPU.h
@@ -3,6 +3,11 @@
 
 #include "IRaycaster.h"
 
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
 
 namespace vr
 {
@@ -17,14 +22,37 @@ private:
 	IRenderer*				m_renderer;
 	ResourceManager*		m_resourceManager;
 
+	uint16					m_width;
+	uint16					m_height;
+
+	std::unique_ptr< uint32[] >		m_renderBuffer;
+
+	std::vector< std::thread >		m_threadPool;
+	std::mutex						m_lock;
+	std::condition_variable_any		m_condition;
+	std::atomic< bool >				m_run;
+	std::atomic< bool >				m_end;
+
+
 protected:
 public:
-	explicit		RaycasterCPU		() = default;
+	explicit		RaycasterCPU		();
 					~RaycasterCPU		() = default;
 
 
 	virtual void	Render				( OctreePtr octree, RenderTargetObject* svoRenderTarget, CameraActor* camera )		override;
-	virtual void	Init				( IRenderer* renderer, ResourceManager* resourceManager )										override;
+	virtual void	Init				( IRenderer* renderer, ResourceManager* resourceManager )							override;
+
+
+private:
+	void			ReallocateRenderBuffer	( uint16 newWidth, uint16 newHeight );
+	void			UpdateRenderTarget		( uint32* buffer, RenderTargetObject* svoRenderTarget );
+	void			SpawnThreads			( OctreePtr octree, CameraActor* camera );
+	void			ThreadWait				();
+
+	void			RaycasterThread			( OctreePtr octree, CameraActor* camera, uint32 startRange, uint32 endRange );
+
+	void			PrepareThreads			();
 };
 
 
