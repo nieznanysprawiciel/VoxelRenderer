3cd72e8ee3761015fb50a44389d8aa02c4690eb1
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-09-12 21:34:27 +0200

    Raycaster GPU coordinates system (camera fix)

--- WorkingDir/Shaders/RaycasterGPU/Camera.hlsl
+++ WorkingDir/Shaders/RaycasterGPU/Camera.hlsl
@@ -24,7 +24,7 @@ float3		ComputeRayPosition		( CameraData cameraData, int screenX, int screenY )
 		float aspect = cameraData.Width / cameraData.Height;
 
 		float xFactor = screenX / ( cameraData.Width / 2 ) - 1.0f;
-		float yFactor = 1.0f - screenY / ( cameraData.Height / 2 );
+		float yFactor = screenY / ( cameraData.Height / 2 ) - 1.0f;
 
 		float3 position = cameraData.Position;
 		position = cameraData.UpVector * yFactor * cameraData.ViewportSize + position;
@@ -43,7 +43,7 @@ float3		ComputeRayDirection		( CameraData cameraData, int screenX, int screenY )
 		float d = cameraData.Width / ( 2 * tan( radians( cameraData.Fov / 2.0f ) ) );
 		float3 cameraAxis = cameraData.Direction * d;
 
-		cameraAxis = cameraData.UpVector * ( cameraData.Height / 2 - screenY ) + cameraAxis;
+		cameraAxis = cameraData.UpVector * ( screenY - cameraData.Height / 2 ) + cameraAxis;
 		cameraAxis = cameraData.RightVector * ( screenX - cameraData.Width / 2 ) + cameraAxis;
 
 		return normalize( cameraAxis );
