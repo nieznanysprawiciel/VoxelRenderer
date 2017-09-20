e61dd5ee91ea603e49a8cdac7dcc6c5a94733ebb
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-09-20 21:28:59 +0200

    Dir::CreateDirectory function added

--- Code/swCommonLib/System/Dir.cpp
+++ Code/swCommonLib/System/Dir.cpp
@@ -6,3 +6,21 @@
 
 #include "Dir.h"
 
+#include <filesystem>
+
+
+namespace filesystem
+{
+
+
+// ================================ //
+//
+bool			Dir::CreateDirectory		( const filesystem::Path& path )
+{
+	if( path.HasFileName() )
+		return std::experimental::filesystem::create_directories( path.GetParent().String() );
+	else
+		return std::experimental::filesystem::create_directories( path.String() );
+}
+
+}
--- Code/swCommonLib/System/Dir.h
+++ Code/swCommonLib/System/Dir.h
@@ -5,6 +5,13 @@
 @copyright File is part of Sleeping Wombat Libraries.
 */
 
+#include "Path.h"
+
+
+
+namespace filesystem
+{
+
 
 /**@brief */
 class Dir
@@ -15,5 +22,9 @@ public:
 	explicit		Dir() = default;
 	~Dir() = default;
 
+
+	static bool			CreateDirectory		( const filesystem::Path& path );
 };
 
+
+}
